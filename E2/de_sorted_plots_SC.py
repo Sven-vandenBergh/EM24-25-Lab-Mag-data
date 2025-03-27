6ef76af994746392f342bad1e045d887bb4da685
Sven van den Bergh <s.m.vandenbergh@gmail.com>
2025-03-27 23:48:53 +0100

    Update and rename e2.py to de_sorted_plots_SC.py

--- E2/de_sorted_plots_SC.py
+++ E2/de_sorted_plots_SC.py
@@ -1,6 +1,7 @@
 ######################
 #---Import-modules---#
 ######################
+from sympy.printing.pretty.pretty_symbology import line_width
 
 import os
 from matplotlib.pyplot import figure, show, subplots
@@ -32,10 +33,25 @@ print(rdata_de.columns)
 z = rdata_de['z (cm)']*1e-2
 z_err = rdata_de['z_err (cm)']*1e-2
 
-
+print(rdata_de[['B trial_1 (parallel mT)', 'B trial_2 (parallel mT)', 'B trial_3 (parallel mT)']])
 #define measurements for parallel case
 Bp_mean = rdata_de[['B trial_1 (parallel mT)', 'B trial_2 (parallel mT)', 'B trial_3 (parallel mT)']].mean(axis=1)*1e-3
 Bp_err = rdata_de[['B trial_1 (parallel mT)', 'B trial_2 (parallel mT)', 'B trial_3 (parallel mT)']].std(axis=1)*1e-3
+
+print(Bp_mean)
+print(Bp_err)
+
+hom_range = []
+for i in range(len(Bp_mean) - 1):
+    # Compute upper and lower bounds of the ratio considering errors
+    R_min = (Bp_mean[i] - Bp_err[i]) / (Bp_mean[i + 1] + Bp_err[i + 1])
+    R_max = (Bp_mean[i] + Bp_err[i]) / (Bp_mean[i + 1] - Bp_err[i + 1])
+
+    # Check if the range overlaps with [0.97, 1.03]
+    if R_min < 1.03 and R_max > 0.97 or 0.97<Bp_mean[i]/Bp_mean[i+1] < 1.03:
+        hom_range.append(Bp_mean[i])
+hom_field_par = np.asarray(hom_range).mean()
+print(hom_field_par)
 
 #combined error parallel case
 sigma1 = np.sqrt(z_err**2 + Bp_err**2)
@@ -61,6 +77,10 @@ ax.errorbar(z, Bp_mean, xerr=z_err, yerr=Bp_err, fmt='o', color='darkslategrey',
             elinewidth=2, capsize=5, markerfacecolor='darkslategrey', markersize=8,
             label='parallel')     #parallel case
 
+#plot coils
+ax.vlines(-0.05, Bp_mean.min(), Bp_mean.max(), linestyles='-.', color='slategray', linewidth=2, label="coils")
+ax.vlines(0.05, Bp_mean.min(), Bp_mean.max(), linestyles='-.', color='slategray', linewidth=2)
+
 #grid and ticks
 ax.grid(True, which='both', axis='both', linestyle='--', color='gray', alpha=0.5)
 ax.tick_params(axis='both', which='major', labelsize=12)
@@ -75,6 +95,10 @@ ax.errorbar(z, Bap_mean, xerr=z_err, yerr=Bap_err, fmt='o', color='#4a728c',
             elinewidth=2, capsize=5, markerfacecolor='#4a728c', markersize=8,
             label='anti-parallel')     #anti-parallel case
 
+#plot coils
+ax.vlines(-0.05, Bap_mean.min(), Bap_mean.max(), linestyles='-.', color='slategray', linewidth=2, label="coils")
+ax.vlines(0.05, Bap_mean.min(), Bap_mean.max(), linestyles='-.', color='slategray', linewidth=2)
+
 #grid and ticks
 ax.grid(True, which='both', axis='both', linestyle='--', color='gray', alpha=0.5)
 ax.tick_params(axis='both', which='major', labelsize=12)
@@ -85,3 +109,6 @@ ax.legend()
 
 fig.tight_layout()
 show()
+
+
+
